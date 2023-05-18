fa6de5c6b13869e240af642b3bd0cd6adde2419e
TheKingVipul <vipulpaighan.1988@gmail.com>
2023-05-18 18:32:50 +0530

    5th Commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import Sub from './components/Sub/Sub';
 import Add from './components/Add/Add';
 import Multi from './components/Multi/Multi';
+import Divis from './components/Divis/Divis';
 
 function App() {
   return (
     <div className="App">
      <h1> First React project</h1>
-     <p> Trying 4th Commit </p>
+     <p> Trying 5th Commit </p>
 
   <Add a='5' b='2' ></Add>
   <Sub a='5' b='2' />
   <Multi a='5' b='2'></Multi>
+  <Divis a='5' b='2'></Divis>
     </div>
   );
 }
---
+++ src/components/Divis/Divis.js
@@ -0,0 +1,12 @@
+const Divis = (props) => {
+    return (
+        <div>
+            <p>This is Divis component</p>
+            <p> Division = {parseInt(props.a) + parseInt(props.b)} </p>
+        </div>
+    )
+}
+
+
+
+export default Divis;
